06d818e10982da28ad7f23cf6e7c7d2f78be4352
Renana Opochinsky <renanakleinman@gmail.com>
2022-07-31 20:50:14 +0300

    Update streamlit_code.py
    dsf

--- streamlit_code.py
+++ streamlit_code.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.title(“A Simple Streamlit Web App”)
+st.title("A Simple Streamlit Web App")
 name = st.text_input("Enter your name", '')
 
 # model_path = "/dsi/gannot-lab/datasets/mordehay/Result/Test"
